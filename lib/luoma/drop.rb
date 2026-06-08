eacac417b0720d91216194001ec42e45bd595717
James Prior <jamesgr.prior@gmail.com>
2026-06-08 11:43:45 +0100

    Add base Drop and Undefined drops

--- lib/luoma/drop.rb
+++ lib/luoma/drop.rb
@@ -2,6 +2,66 @@
 
 module Luoma
   class Drop
-    # TODO:
+    include Enumerable
+
+    #: (untyped, RenderContext) -> bool
+    def key?(obj, context)
+      false
+    end
+
+    #: (String, RenderContext) -> untyped
+    def [](name, context)
+      :nothing
+    end
+
+    def each
+      Enumerator.new {}
+    end
+
+    #: (untyped, RenderContext) -> bool
+    def eq?(obj, context)
+      false
+    end
+
+    #: (untyped, RenderContext) -> bool
+    def lt?(obj, context)
+      false
+    end
+
+    # Return the length of this object.
+    # Along with `#slice`,  `#length` is part of the iterator protocol.
+    #: (RenderContext) -> Integer
+    def length(context)
+      0
+    end
+
+    #: (Integer?, Integer?, bool?) -> Enumerable[untyped]
+    def slice(offset, limit, reversed)
+      self
+    end
+
+    #: (:data | :numeric | :string | :boolean, RenderContext) -> untyped
+    def to_primitive(hint, context)
+      case hint
+      when :data
+        nil
+      when :numeric
+        0
+      when :string
+        ""
+      when :boolean
+        false
+      end
+    end
+
+    #: () -> String
+    def to_s
+      ""
+    end
+
+    #: () -> String?
+    def to_html_safe_s
+      nil
+    end
   end
 end
--- lib/luoma/drops/undefined.rb
+++ lib/luoma/drops/undefined.rb
@@ -1,6 +1,97 @@
 # frozen_string_literal: true
 
 module Luoma
+  attr_reader :path
+
+  # The default and base "undefined" type.
   class UndefinedDrop < Drop
+    #: (String, t_token, String, String) -> void
+    def initialize(path, token, source, template_name)
+      super()
+      @path = path
+      @token = token
+      @source = source
+      @template_name = template_name
+    end
+
+    #: (String, RenderContext) -> untyped
+    def [](name, context)
+      self
+    end
+
+    #: (untyped, RenderContext) -> bool
+    def eq?(obj, context)
+      obj.nil? || obj.is_a?(UndefinedDrop) || obj.is_a?(BlankDrop)
+    end
+  end
+
+  # An "undefined" type that raises `UndefinedVariableError` in all contexts.
+  class StrictUndefinedDrop < UndefinedDrop
+    def key?(obj, context)
+      error
+    end
+
+    def [](name, context)
+      error
+    end
+
+    def each
+      error
+    end
+
+    def eq?(obj, context)
+      error
+    end
+
+    def lt?(obj, context)
+      error
+    end
+
+    def length(context)
+      error
+    end
+
+    def slice(offset, limit, reversed)
+      error
+    end
+
+    def to_primitive(hint, context)
+      error
+    end
+
+    def to_s
+      error
+    end
+
+    def to_html_safe_s
+      error
+    end
+
+    protected
+
+    def error
+      raise UndefinedVariableError.new(
+        "#{@path.inspect} is undefined",
+        @token,
+        @source,
+        @template_name
+      )
+    end
+  end
+
+  # An "undefined" type that can be tested for truthiness and compared to
+  # other objects without raising an error.
+  class FalsyStrictUndefinedDrop < StrictUndefinedDrop
+    def eq?(obj, context)
+      context.env.nothing?(obj)
+    end
+
+    def to_primitive(hint, context)
+      hint == :boolean ? false : error
+    end
+
+    def key?(obj, context)
+      false
+    end
   end
 end
