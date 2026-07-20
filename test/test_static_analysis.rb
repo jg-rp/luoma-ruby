c8f6e2d138a1b891c51bd79f5f396aa4169930e8
James Prior <jamesgr.prior@gmail.com>
2026-07-20 19:40:12 +0100

    More static analysis tests

--- test/test_static_analysis.rb
+++ test/test_static_analysis.rb
@@ -55,4 +55,153 @@ class TestStaticAnalysis < Minitest::Test
     assert_locations(analysis.filters, filters)
     assert_locations(analysis.tags, tags)
   end
+
+  def test_bracket_notation
+    source = "{{ x['y'].title }}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {}
+    globals = {
+      "x" => [[%w[x y title], "x['y'].title"]]
+    }
+    variables = globals
+    filters = {}
+    tags = {}
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
+
+  def test_quoted_dot_notation
+    source = "{{ some['foo.bar'] }}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {}
+    globals = {
+      "some" => [[["some", "foo.bar"], "some['foo.bar']"]]
+    }
+    variables = globals
+    filters = {}
+    tags = {}
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
+
+  def test_nested_queries
+    source = "{{ x[y.z].title }}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {}
+    globals = {
+      "x" => [[["x", %w[y z], "title"], "x[y.z].title"]],
+      "y" => [[%w[y z], "y.z"]]
+    }
+    variables = globals
+    filters = {}
+    tags = {}
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
+
+  # def test_dynamic_root_query
+  #   source = "{{ [a.b] }}"
+  #   analysis = Luoma.parse(source).analyze
+
+  #   locals = {}
+  #   globals = {
+  #     "a.b" => [[[%w[a b]], "a.b"]],
+  #     "a" => [[%w[a b], "a.b"]]
+  #   }
+  #   variables = globals
+  #   filters = {}
+  #   tags = {}
+
+  #   assert_vars(analysis.locals, locals)
+  #   assert_vars(analysis.globals, globals)
+  #   assert_vars(analysis.variables, variables)
+  #   assert_locations(analysis.filters, filters)
+  #   assert_locations(analysis.tags, tags)
+  # end
+
+  def test_variable_segments
+    source = "{{ a['b.c'] }}{{ d[e.f][4] }}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {}
+    globals = {
+      "a" => [[["a", "b.c"], "a['b.c']"]],
+      "d" => [[["d", %w[e f], 4], "d[e.f][4]"]],
+      "e" => [[%w[e f], "e.f"]]
+    }
+    variables = globals
+    filters = {}
+    tags = {}
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
+
+  def test_assign
+    source = "{% assign x = y | append: z %}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {
+      "x" => [[["x"], "x"]]
+    }
+    globals = {
+      "y" => [[["y"], "y"]],
+      "z" => [[["z"], "z"]]
+    }
+    variables = globals
+    filters = {
+      "append" => ["append: z"]
+    }
+    tags = {
+      "assign" => ["assign"]
+    }
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
+
+  def test_multi_assign
+    source = "{% assign x = y, a = 2 %}"
+    analysis = Luoma.parse(source).analyze
+
+    locals = {
+      "x" => [[["x"], "x"]],
+      "a" => [[["a"], "a"]]
+    }
+    globals = {
+      "y" => [[["y"], "y"]]
+    }
+    variables = globals
+    filters = {}
+    tags = {
+      "assign" => ["assign"]
+    }
+
+    assert_vars(analysis.locals, locals)
+    assert_vars(analysis.globals, globals)
+    assert_vars(analysis.variables, variables)
+    assert_locations(analysis.filters, filters)
+    assert_locations(analysis.tags, tags)
+  end
 end
